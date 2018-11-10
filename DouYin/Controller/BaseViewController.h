330d92480d3307ba8774fb93d11bf9ba4b7a7114
Grivia <694463863@qq.com>
2018-11-10 09:34:29 +0800

    small changes

--- DouYin/Controller/BaseViewController.h
+++ DouYin/Controller/BaseViewController.h
@@ -12,6 +12,38 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface BaseViewController : UIViewController
 
+- (void) initNavigationBarTransparent;
+
+- (void) setBackgroundColor:(UIColor *)color;
+
+- (void) setTranslucentCover;
+
+- (void) initLeftBarButton:(NSString *)imageName;
+
+- (void) setStatusBarHidden:(BOOL) hidden;
+
+- (void) setStatusBarBackgroundColor:(UIColor *)color;
+
+- (void) setNavigationBarTitle:(NSString *)title;
+
+- (void) setNavigationBarTitleColor:(UIColor *)color;
+
+- (void) setNavigationBarBackgroundColor:(UIColor *)color;
+
+- (void) setNavigationBarBackgroundImage:(UIImage *)image;
+
+- (void) setStatusBarStyle:(UIStatusBarStyle)style;
+
+- (void) setNavigationBarShadowImage:(UIImage *)image;
+
+- (void) back;
+
+- (CGFloat) navagationBarHeight;
+
+- (void) setLeftButton:(NSString *)imageName;
+
+- (void) setBackgroundImage:(NSString *)imageName;
+
 @end
 
 NS_ASSUME_NONNULL_END
